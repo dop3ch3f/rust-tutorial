76081bfadbaf49e217ecc243140c687620c22f57
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-02 12:34:11 +0100

    data types

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,23 @@ fn main() {
     let z = x + y;
     // heres how you print out
     println!("z is  {}", z);
+
+    // Booleans
+    let a = true;
+    if a {
+        println!("true");
+    }
+    // array
+    let a = [100, 200, 300];
+    // usize
+    let b = a[0];
+    // f64 for devs
+    let x = 54.3;
+    let p = 'a';
+    // tuples
+    let tup = (1, 'c', true);
+    // accessing tuples
+    println!("{}", tup.0);
+    // destructuring tuples
+    let (x, y, z) = tup;
 }
