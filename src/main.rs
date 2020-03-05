5d1cecfd6ecbd1a6ac3ae061e844ce4d23b16b1b
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-05 19:29:51 +0100

    custom data types Enums

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,26 @@
-use std::io;
+// Properties of enums
+// can only be one value at a time
+// can list (enumerate) all possible values
+// do not change on run time
 
-// entry point for all programs
+// defining enum variants that hold data
+enum Clock {
+    Sundial(u8),
+    Digital(u8, u8),
+    Analog(u8, u8, u8),
+}
+
+fn tell_time(clock: Clock) {
+    match clock {
+        Clock::Sundial(hours) => println!("{} o'clock", hours),
+        Clock::Analog(hours, minutes, seconds) => println!("{}:{}:{}", hours, minutes, seconds),
+        Clock::Digital(hours, minutes) => println!("{}:{}", hours, minutes),
+    }
+}
+
 fn main() {
-    next_birthday("ifeanyi ibekie", 33);
-    println!("The square of 3 is {}", square(3));
-    discount(10);
-    // loop
-    loop {
-        println!("Whats the secret word?");
-        let mut word = String::new();
-        io::stdin()
-            .read_line(&mut word)
-            .expect("Failed to read Line");
-
-        if word.trim() == "rust" {
-            println!("Genius youve got it");
-            break;
-        }
-    }
-
-    // while example
-    let mut word = String::new();
-    while word.trim() != "rust" {
-        println!("Whats the secret word?");
-        word = String::new();
-        io::stdin()
-            .read_line(&mut word)
-            .expect("Failed to read Line");
-    }
-    // for loops
-    for i in 1..3 {
-        println!("Now serving number {}", i);
-    }
-
-    // match
-    let x = 3;
-    match x {
-        1 => println!("matched one"),
-        2 => println!("matched two"),
-        3 => println!("matched three"),
-        _ => println!("matched another number"),
-    }
-
-    // concrete match example
-    let die1 = 1;
-    let die2 = 5;
-
-    match (die1, die2) {
-        (1,1) => println!("Go to begining"),
-        (5, _) | (_, 5) => {
-            println!("you rolled atleast a 5");
-        },
-        _ => println!("you can move")
-    }
-
-    // exhaustive checking rust example
-
-    let is_confirmed = true;
-    let is_active = false;
-
-    match (is_confirmed, is_active) {
-        (true, true) => println!("this account is in good standing"),
-        (false, true) => println!("you will need to confirm your account"),(false, false) => println!("this account will be deactivated"),
-        (true, false) => println!("this is the final case and you are active but not confirmed"),
-    }
-}
-
-/// fn name(param1: type1, ...) -> return_type {
-///   ...body...
-/// }
-
-fn next_birthday(name: &str, current_age: u8) {
-    let next_age = current_age + 1;
-    println!(
-        "Hi {}, on your next birthday, you will be {}",
-        name, next_age
-    );
-}
-
-fn square(num: isize) -> isize {
-    num * num
-}
-
-fn discount(day_of_the_month: u8) {
-    let amount = if day_of_the_month % 2 == 0 { 50 } else { 10 };
-
-    println!("Your discount is {}%", amount);
+    // using an enum
+    tell_time(Clock::Analog(9, 25, 30));
+    tell_time(Clock::Digital(8, 25));
+    tell_time(Clock::Sundial(10));
 }
