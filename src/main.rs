3f2ec1c20bc3359ffcb78c8fe476c7bfac5c1a33
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-07 12:10:05 +0100

    Borrowing and Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,55 @@
+// how to create and use a mutable reference
+
+// allows us to print out the values of the struct for inspection
+#[derive(Debug)]
+struct Bucket {
+    liters: u32,
+}
+
+#[derive(Debug)]
+struct CarPool {
+    passengers: Vec<String>,
+}
+
+// in first parameters of methods
+impl CarPool {
+    /// Add the named passenger to the carpool
+    fn pick_up(&mut self, name: String) {
+        self.passengers.push(name);
+    }
+}
+
+// in functions
+fn pour(source: &mut Bucket, target: &mut Bucket, amount: u32) {
+    source.liters -= amount;
+    target.liters += amount;
+}
+
 fn main() {
-    // let v = vec![10,20,30];
-    // let v_slice = &v[..9];
-    // println!("v_slice is: {:?}", v_slice);
+    let mut bucket1 = Bucket { liters: 20 };
+    let mut bucket2 = Bucket { liters: 10 };
 
-    let a = [1, 2, 3];
-    let v = vec![4, 5, 6];
-    // how to create a slice
-    let v_slice = &v[..];
+    pour(&mut bucket1, &mut bucket2, 3);
 
-    only_reference_to_array(&a);
-    only_reference_to_vector(&v);
-    reference_to_either_array_or_vectors(v_slice);
-    reference_to_either_array_or_vectors(&a);
+    println!("Bucket 1: {:?}", bucket1);
+    println!("Bucket 2: {:?}", bucket2);
 
-    let s = String::from("hi");
-    let string_literal = "hello";
-    // passing a reference to a string or string literal will instantly be de referenced to a string slice on the receiving end
-    either_string_or_literal(&s);
-    either_string_or_literal(&string_literal);
+    let mut monday_car_pool = CarPool { passengers: vec![] };
+
+    monday_car_pool.pick_up(String::from("Ifeanyi"));
+    println!("Carpool state: {:?}", monday_car_pool);
+
+    monday_car_pool.pick_up(String::from("Dayo"));
+    println!("Carpool state: {:?}", monday_car_pool);
+
+    // Only one mutable reference is allowed at a time or multiple immutable reference to a value
+
+    // an example of iterator invalidation bug
+    //  let mut list = vec![1,2,3];
+    //  for i in &list {
+    //      println!("i is {}", i);
+    //      list.push(i+1);
+    //  }
 }
-
-fn either_string_or_literal(param: &str) {
-    println!("this is a string slice: {:?}", param);
-}
-
-fn only_reference_to_array(param: &[i32; 3]) {
-    println!("this is an array: {:?}", param);
-}
-
-fn only_reference_to_vector(param: &Vec<i32>) {
-    println!("this is a vector: {:?}", param);
-}
-
-fn reference_to_either_array_or_vectors(param: &[i32]) {
-    println!("this is a slice: {:?}", param);
-}
+// borrowing rules involving mutability
+// example of a problem prevented by the borrowing rules
