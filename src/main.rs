242c4d7f71708feb0646d1ab763f53f7c4b79093
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-15 02:07:09 +0100

    Error handling in rust

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
-// How to return Result
+// Seperating bugs from recoverable errors
 
-use std::time;
+// Panic
+// - bad state that shouldn't happen
+// - no recovery
+// - bug that a programmer must fix
 
-fn save_status(text: &str) -> Result<i64, &'static str> {
-    if text.len() > 200 {
-        return Err("status text is too long");
-    }
-// How to use ?
-    let record = save_to_database(text)?;
+// Result
+// - Bad state that might happen in normal use
+// - recovery is a possiblity
+// - end user might be able to fix
 
-    Ok(record.id)
+// Advantages from rusts error handling
+// - loud failures
+// - fail fasts (prevents hacking and easy debugs)
+// - enforce assumptions (the expect method helps to easily panic with a value on methods that return a type result and option)
+//   let config = File::open("config.toml").expect("Config file not created on startup")
 
-}
+// - possibility of errors is clear. once you see a result it can fail
+// - compiler forces handling
 
-fn save_to_database(text: &str) -> Result<StatusRecord, &'static str> {
-    // fake implementation that always fails
-    Err("db unavailable")
-}
+// // in contrast with go
+// // return (success value, error value) not one or another
+// // check wether the error value is nil after each function call
+// // no compiler enforcement
 
-struct StatusRecord {
-    id: i64,
-    text: String,
-    created_at: std::time::Instant,
-}
-
-// Where ? is valid
-// - it must be called in methods that return a result or an option so you can annotate methods that didnt initially to let the code pass
-// Recent features involving ?
-
-// - you can use ? on Option Values
-// - same but instead return Some or None
-// - can use ? to return in main because now main can return a value just anotate the return type
-// - you can use question mark in tests the same way
+// Disadvantages
+// -it can be annoying to always return errors
+// - use expect a lot
