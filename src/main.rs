4d47ae72add4385cf6910caf985bbe2218959d3d
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-16 02:32:24 +0100

    using quick_error crate

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
-// Custom Error types
-
-// Solution 1: Returning a Box<Error> trait object
-// use std::env;
-// use std::error::Error;
-// fn num_threads() -> Result<usize, Box<dyn Error>> {
-//     let s = env::var("NUM_THREADS")?;
-//     let n: usize = s.parse()?;
-//     Ok(n + 1)
-// }
-
-// fn run_application() -> Result<(), Box<dyn Error>> {
-//     let num = num_threads()?;
-//     println!("the number of threads is {}", num);
-//     Ok(())
-// }
-
-// fn main() {
-//     if let Err(e) = run_application() {
-//         panic!("an error occured: {}", e);
-//     }
-// }
-
-// Downside
-// - Can't inspect the error type in code
-// - Can't decide to handle different errors differently
-
-// Solution 2: Defining a custom error type
-
 // Document Storage Service as example
+#[macro_use]
+extern crate quick_error;
 
-use std::error::Error;
-use std::fmt;
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::io;
+// quick-error features
+// Macro: quick_error!
+// Implements Error trait
+// Easier Display trait implementation
+// Easier From trait implementations
+// Easily add context
+
+use quick_error::ResultExt;
+use std::fs::{File, OpenOptions};
+use std::{io, result};
 
 const MAX_DOCS_CREATES_PER_MINUTE: u8 = 100;
 
@@ -41,33 +19,21 @@ fn num_documents_created_in_last_minute() -> u8 {
     2
 }
 
-#[derive(Debug)]
-pub enum DocumentServiceError {
-    RateLimitExceeded,
-    Io(io::Error),
-}
-
-impl Error for DocumentServiceError {}
-
-impl fmt::Display for DocumentServiceError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use DocumentServiceError::*;
-        match *self {
-            RateLimitExceeded => write!(f, "Max number of docs per minute"),
-            Io(ref io) => write!(f, "I/O error: {}", io),
+quick_error! {
+    #[derive(Debug)]
+    pub enum DocumentServiceError {
+        RateLimitExceeded {
+            display("You have exceeded the allowed number of documents per minute")
+        }
+        Io(filename: String, cause: io::Error) {
+            display("I/O error: {} for filename {}", cause, filename)
+            context(filename: &'a str, cause: io::Error)
+                -> (filename.to_string(), cause)
         }
     }
 }
 
-impl From<io::Error> for DocumentServiceError {
-    fn from(other: io::Error) -> Self {
-        DocumentServiceError::Io(other)
-    }
-}
-
 // Result type alias
-use std::result;
-
 pub type Result<T> = result::Result<T, DocumentServiceError>;
 
 pub fn create_document(filename: &str) -> Result<File> {
@@ -78,9 +44,23 @@ pub fn create_document(filename: &str) -> Result<File> {
     let file = OpenOptions::new()
         .write(true)
         .create_new(true)
-        .open(filename)?;
+        .open(filename)
+        .context(filename)?;
 
     Ok(file)
 }
 
-fn main() {}
+fn create_project(project_name: &str) -> Result<()> {
+    create_document(&format!("{}-draft1", project_name))?;
+    create_document(&format!("{}-draft2", project_name))?;
+    create_document(&format!("{}-draft3", project_name))?;
+    create_document(&format!("{}-draft4", project_name))?;
+    Ok(())
+}
+
+fn main() {
+    match create_project("new-project") {
+        Ok(()) => println!("Project created successfully"),
+        Err(e) => println!("Project creation failed: {}", e),
+    }
+}
