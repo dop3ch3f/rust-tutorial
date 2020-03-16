e2e05619d2e5f0568434b8a9713ede1b2c765219
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-16 12:32:28 +0100

    useful methods on result and option

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,13 @@
-extern crate failure;
-#[macro_use]
-extern crate failure_derive;
+// Useful methods on result and option
 
-use failure::{Backtrace, Fail};
-use std::fs::{File, OpenOptions};
-use std::{io, result};
+// Test which variant
+// is_ok, is_err, is_some, is_none
 
-type Result<T> = result::Result<T, DocumentServiceError>;
+// Converting Result->Option, Option->Result
+// ok, ok_or
 
-#[derive(Debug, Fail)]
-pub enum DocumentServiceError {
-    #[fail(display = "You have exceeded the allowed number of documents per minute.")]
-    RateLimitExceeded(Backtrace),
-    #[fail(display = "I/O error: {}", _0)]
-    Io(io::Error, Backtrace),
-}
+// Fallback values
+// unwrap_or, unwrap_or_default
 
-impl From<io::Error> for DocumentServiceError {
-    fn from(other: io::Error) -> Self {
-        DocumentServiceError::Io(other, Backtrace::new())
-    }
-}
-
-const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
-
-fn num_documents_created_in_last_minute() -> u8 {
-    2
-}
-
-pub fn create_document(filename: &str) -> Result<File> {
-    if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
-        return Err(DocumentServiceError::RateLimitExceeded(Backtrace::new()));
-    }
-
-    let file = OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(filename)?;
-
-    Ok(file)
-}
-
-fn create_project(project_name: &str) -> Result<()> {
-    create_document(&format!("{}-draft1", project_name))?;
-    create_document(&format!("{}-draft2", project_name))?;
-    create_document(&format!("{}-revision1", project_name))?;
-    create_document(&format!("{}-revision2", project_name))?;
-    Ok(())
-}
-
-fn main() {
-    match create_project("my-project") {
-        Ok(()) => println!("Project created successfully"),
-        Err(e) => {
-            println!("Project creation failed: {}", e);
-            if let Some(backtrace) = e.backtrace() {
-                if !backtrace.to_string().trim().is_empty() {
-                    println!("backtrace: {:?}", backtrace);
-                }
-            }
-        }
-    }
-}
+// Transform Ok or Some values only
+// map
