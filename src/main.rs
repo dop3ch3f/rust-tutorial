32d2d850d4193ecc6f7e17583610dd3f591d35dc
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-16 02:55:43 +0100

    failure error crate

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,42 @@
-// Document Storage Service as example
+extern crate failure;
 #[macro_use]
-extern crate error_chain;
+extern crate failure_derive;
 
-// error chain features
-// macro error_chain
-// Easier display trait implementation
-// easier from trait implementations
-// implements error trait
-// opinionated type definitions without boilerplate
-// result type alias
-// add context through a chain of errors
-// Backtrace support
-
+use failure::{Backtrace, Fail};
 use std::fs::{File, OpenOptions};
+use std::{io, result};
 
-const MAX_DOCS_CREATES_PER_MINUTE: u8 = 100;
+type Result<T> = result::Result<T, DocumentServiceError>;
+
+#[derive(Debug, Fail)]
+pub enum DocumentServiceError {
+    #[fail(display = "You have exceeded the allowed number of documents per minute.")]
+    RateLimitExceeded(Backtrace),
+    #[fail(display = "I/O error: {}", _0)]
+    Io(io::Error, Backtrace),
+}
+
+impl From<io::Error> for DocumentServiceError {
+    fn from(other: io::Error) -> Self {
+        DocumentServiceError::Io(other, Backtrace::new())
+    }
+}
+
+const MAX_DOCS_CREATED_PER_MINUTE: u8 = 100;
 
 fn num_documents_created_in_last_minute() -> u8 {
     2
 }
 
-pub mod errors {
-    error_chain! {
-        errors {
-            RateLimitExceeded {
-                display("Maximum files")
-            }
-        }
-        foreign_links {
-            Io(::std::io::Error);
-        }
-    }
-}
-
-use errors::*;
-
 pub fn create_document(filename: &str) -> Result<File> {
-    if num_documents_created_in_last_minute() > MAX_DOCS_CREATES_PER_MINUTE {
-        bail!(ErrorKind::RateLimitExceeded);
+    if num_documents_created_in_last_minute() > MAX_DOCS_CREATED_PER_MINUTE {
+        return Err(DocumentServiceError::RateLimitExceeded(Backtrace::new()));
     }
 
     let file = OpenOptions::new()
         .write(true)
         .create_new(true)
-        .open(filename)
-        .chain_err(|| format!("could not open {}", filename))?;
+        .open(filename)?;
 
     Ok(file)
 }
@@ -52,21 +44,20 @@ pub fn create_document(filename: &str) -> Result<File> {
 fn create_project(project_name: &str) -> Result<()> {
     create_document(&format!("{}-draft1", project_name))?;
     create_document(&format!("{}-draft2", project_name))?;
-    create_document(&format!("{}-draft3", project_name))?;
-    create_document(&format!("{}-draft4", project_name))?;
+    create_document(&format!("{}-revision1", project_name))?;
+    create_document(&format!("{}-revision2", project_name))?;
     Ok(())
 }
 
 fn main() {
-    match create_project("new-project") {
+    match create_project("my-project") {
         Ok(()) => println!("Project created successfully"),
         Err(e) => {
             println!("Project creation failed: {}", e);
-            for e in e.iter().skip(1) {
-                println!("Caused by: {}", e)
-            }
             if let Some(backtrace) = e.backtrace() {
-                println!("backtrace: {:?}", backtrace);
+                if !backtrace.to_string().trim().is_empty() {
+                    println!("backtrace: {:?}", backtrace);
+                }
             }
         }
     }
