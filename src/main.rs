48ed31adccf0cc95a0dd23f3dd6729fd5e823bcc
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-30 03:10:58 +0100

    Concrete Lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-// Useful methods on result and option
+// Concrete Lifetimes
 
-// Test which variant
-// is_ok, is_err, is_some, is_none
+// What is a concrete lifetime of values and references
 
-// Converting Result->Option, Option->Result
-// ok, ok_or
+// A lifetime is the time a value exists at one memory address e.g a real persons address
+// Start: when a value is created or moved into a location in memory
+// End: Moved or dropped from that location
+// values and references are the same in operation but the added constraint for references is that their value must be container withing the referenced values's lifetime
 
-// Fallback values
-// unwrap_or, unwrap_or_default
 
-// Transform Ok or Some values only
-// map
+// Visualize concrete lifetimes of values and references in code
+// - within one function
+
+// fn main() {
+//     let list = vec![100,34,72,55];
+//     let first_two = &list[0..2];
+//     println!("first two are {:?}", first_two );
+//     println!("list is {:?}", list);
+//     println!("again, first two: {:?}", first_two );
+// }
+
+// - across multiple functions
+
+fn return_first_two(borrowed_list: &[i32]) -> &[i32] {
+    &borrowed_list[0..2]
+}
+
+fn main() {
+    let list = vec![100,34,72, 55];
+    let first_two = return_first_two(&list);
+    println!("first two are {:?}", first_two );
+    println!("list is {:?}", list);
+    println!("again, first two: {:?}", first_two );
+}
