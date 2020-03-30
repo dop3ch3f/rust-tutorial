8ad7bb7a3fbc61656e51504eab173f70cfc35eb3
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-03-30 04:39:19 +0100

    visualizing lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,74 @@
-// Concrete Lifetimes
+// Visualizing lifetimes
 
-// What is a concrete lifetime of values and references
+// Examples of invalid references
 
-// A lifetime is the time a value exists at one memory address e.g a real persons address
-// Start: when a value is created or moved into a location in memory
-// End: Moved or dropped from that location
-// values and references are the same in operation but the added constraint for references is that their value must be container withing the referenced values's lifetime
-
-
-// Visualize concrete lifetimes of values and references in code
-// - within one function
+// Returning a reference to a value created within an inner scope
 
 // fn main() {
-//     let list = vec![100,34,72,55];
-//     let first_two = &list[0..2];
-//     println!("first two are {:?}", first_two );
-//     println!("list is {:?}", list);
-//     println!("again, first two: {:?}", first_two );
+//     let first_two = {
+//         let list = vec![100, 34, 72, 55];
+//         &list[0..2]
+//     };
+
+//     println!("First two are {:?}", first_two);
+// }
+ 
+
+// Returning a reference to a value created within a function
+
+// fn main() {
+//     let first_two = return_first_two();
+//     println!("First two are {:?}", first_two );
 // }
 
-// - across multiple functions
+// fn return_first_two() -> &[i32] {
+//     let list = vec![100, 34, 72, 55];
+//     &list[0..2]
+// }
 
-fn return_first_two(borrowed_list: &[i32]) -> &[i32] {
-    &borrowed_list[0..2]
-}
+// Returning a moved value
 
-fn main() {
-    let list = vec![100,34,72, 55];
-    let first_two = return_first_two(&list);
-    println!("first two are {:?}", first_two );
-    println!("list is {:?}", list);
-    println!("again, first two: {:?}", first_two );
-}
+// fn main() {
+//     let list_a = vec![100, 34, 72, 55];
+//     let list_b = list_a;
+//     let first_two = &list_a[0..2];
+//     println!("First two are {:?}", first_two );
+// }
+
+// Self referential struct
+
+// struct ListAndRef {
+//     list: Vec<i32>,
+//     first_two: &[i32],
+// }
+
+// fn return_list_and_first_two() -> ListAndRef {
+//     let list_to_use = vec![100, 34, 72, 55];
+
+//     ListAndRef {
+//         list: list_to_use,
+//         first_two: &list_to_use[0..2],
+//     }
+// }
+
+// to enforce self referential structs checkout crates
+// rental, owning-ref
+
+// Storing references in a HashMap
+
+// use std::io;
+// use std::collections::HashMap;
+
+// fn main() {
+//     println!("Please enter some text to get word counts");
+//     let mut counts = HashMap::new();
+//     let mut input = String::new();
+
+//     io::stdin().read_line(&mut input).expect("Problem reading input");
+
+//     for word in input.split_whitespace() {
+//         let count = counts.entry(word).or_insert(0);
+//         *count += 1;
+//     }
+//     println!("counts = {:?}", counts );
+// }
