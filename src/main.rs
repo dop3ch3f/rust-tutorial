ede6ffdc6f291683f25fa0971ba3101b313cba9b
dop3ch3f <ifeanyi.ibekie@gmail.com>
2020-04-03 06:22:57 +0100

    Lifetime elision

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
-// Lifetime parameters are descriptive
-    // - Describe intended relationships between references
-    // - Concrete lifetimes that fill in the parameters are determined by the code calling the definitions containing lifetime parameters
+// Lifetime elision definition
+    // Set of rules in the compiler
+        // if rules aren't enough to figure out lifetime parameters, compiler will error
+        // Override defaults by adding lifetime parameters
+        // Can annotate lifetimes if you want
 
-// Lifetime parameters are not prescriptive
-    // - Don't prescribe how long an input reference or value actually lives
-    // - Don't change how long an input reference or value actually lives
-    // - Can't use lifetime parameters to set/assign/force a lifetime
-    // - Can't make a reference live long enough
+// Three lifetime elision rules
+    // in parameters, each reference gets its own lifetime
+    // if theres is one reference in the parameters, the returned reference gets that lifetime
+    //(Methods only) if there is a &self or &mut self parameter, returned reference gets that lifetime
 
-// 'static lifetime
-    // Only concrete lifetime you can specify
-    // References valid during the whole program
-    // Similar to static variables
-    // References to static variables, constants, literals
-
-// Questions to ask when fixing lifetime-related errors
-    // What are the relationships in the implementation
-    // What are the concrete lifetimes where the definition is used
-    // How do i reogranize the code so the references are always valid
-    // Would owned values be more appropriate than references
+// Lifetime elision in previous modules examples
